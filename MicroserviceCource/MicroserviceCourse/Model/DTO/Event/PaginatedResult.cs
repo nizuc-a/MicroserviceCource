35ccf46c2d44c87537c14f5aeed5f6dbbfd79461
nizuc <nizenko@t-soft.ru>
2026-05-08 08:25:13 +0300

    Добавил в Event два поля TotalSeats и AvailableSeats, а так же два метода TryReserveSeats и ReleaseSeats. В AddEventDto добавил поле TotalSeats.

--- MicroserviceCource/MicroserviceCourse/Model/DTO/Event/PaginatedResult.cs
+++ MicroserviceCource/MicroserviceCourse/Model/DTO/Event/PaginatedResult.cs
@@ -4,6 +4,6 @@ public class PaginatedResult
 {
     public int AllElementCount { get; set; }
     public int Page { get; set; }
-    public Entity.Event[] Events { get; set; }
+    public Entity.Event[] Events { get; set; } = [];
     public int CurrentPageElementCount { get; set; }
 }
--- MicroserviceCource/MicroserviceCourse/Model/Entity/Event.cs
+++ MicroserviceCource/MicroserviceCourse/Model/Entity/Event.cs
@@ -2,12 +2,17 @@
 
 public class Event
 {
-    public Event(string title, string description, DateTime startAt, DateTime endAt)
+    private readonly object _locker = new object();
+
+    public Event(string title, string description, DateTime startAt, DateTime endAt, int totalSeats,
+        int? availableSeats = null)
     {
         Title = title;
         Description = description;
         StartAt = startAt;
         EndAt = endAt;
+        TotalSeats = totalSeats;
+        AvailableSeats = availableSeats ?? totalSeats;
     }
 
     public Guid Id { get; set; }
@@ -15,12 +20,40 @@ public class Event
     public string Description { get; set; }
     public DateTime StartAt { get; set; }
     public DateTime EndAt { get; set; }
+    public int TotalSeats { get; set; }
+    public int AvailableSeats { get; set; }
 
-    public void Update(string title, string description, DateTime startAt, DateTime endAt)
+    public void Update(string title, string description, DateTime startAt, DateTime endAt, int totalSeats,
+        int availableSeats)
     {
         Title = title;
         Description = description;
         StartAt = startAt;
         EndAt = endAt;
+        TotalSeats = totalSeats;
+        AvailableSeats = availableSeats;
+    }
+
+    public bool TryReserveSeats(int count = 1)
+    {
+        lock (_locker)
+        {
+            if (AvailableSeats < count)
+                return false;
+
+            AvailableSeats -= count;
+            return true;
+        }
+    }
+
+    public void ReleaseSeats(int count = 1)
+    {
+        lock (_locker)
+        {
+            if (AvailableSeats + count >= TotalSeats)
+                throw new ArgumentOutOfRangeException(nameof(count));
+            
+            AvailableSeats += count;
+        }
     }
 }
